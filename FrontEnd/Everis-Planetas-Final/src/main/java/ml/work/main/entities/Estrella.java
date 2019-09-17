4b1f9d309930abf1fcfafb58e5c416ed7f9a87b8
Fabrizio <f.pouget94@gmail.com>
2019-09-16 23:23:30 -0300

    Codificacion Entities
    Se modificaron las clases Entities para poder realizar el mapeado en la Base de Datos.

--- FrontEnd/Everis-Planetas-Final/src/main/java/ml/work/main/entities/Estrella.java
+++ FrontEnd/Everis-Planetas-Final/src/main/java/ml/work/main/entities/Estrella.java
@@ -1,5 +1,74 @@
 package ml.work.main.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "api_Estrella")
 public class Estrella {
 
+	// Atributos - Campos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_Estrella")
+	protected Long id;
+	
+	@Column(name = "nombre_Estrella")
+	protected String nombre;
+	
+	@Column(name = "densidad_Estrella")
+	protected Long densidad;
+
+	// Constructores
+	public Estrella() {
+		super();
+	}
+
+	public Estrella(Long id) {
+		super();
+		this.id = id;
+	}
+
+
+	public Estrella(Long id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public Estrella(Long id, String nombre, Long densidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.densidad = densidad;
+	}
+	
+	// Getters & Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Long getDensidad() {
+		return densidad;
+	}
+
+	public void setDensidad(Long densidad) {
+		this.densidad = densidad;
+	}
+	
 }
--- FrontEnd/Everis-Planetas-Final/src/main/java/ml/work/main/entities/Planeta.java
+++ FrontEnd/Everis-Planetas-Final/src/main/java/ml/work/main/entities/Planeta.java
@@ -1,5 +1,74 @@
 package ml.work.main.entities;
 
-public class Planeta {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity(name = "api_Planeta")
+public class Planeta {
+	
+	//Atributos - Campos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_Planeta")
+	protected Long id;
+	
+	@Column(name = "nombre_Planeta")
+	protected String nombre;
+	
+	@Column(name = "superficie_Planeta")
+	protected int superficie;
+
+	// Constructores
+	
+	public Planeta() {
+		super();
+	}
+
+	public Planeta(Long id) {
+		super();
+		this.id = id;
+	}
+
+	public Planeta(String nombre, int superficie) {
+		super();
+		this.nombre = nombre;
+		this.superficie = superficie;
+	}
+	
+	public Planeta(Long id, String nombre, int superficie) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.superficie = superficie;
+	}
+
+	// Getters & Setters
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(int superficie) {
+		this.superficie = superficie;
+	}
 }
